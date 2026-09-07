f87501b5fd699783bb307d63d5bf0e25f9c8f778
Dilip9 <dilip.verma940805@gmail.com>
2026-09-08 00:25:30 +0530

    Adde custom new exception status code

--- src/main/java/gouri/JL/utility/JLStatusCode.java
+++ src/main/java/gouri/JL/utility/JLStatusCode.java
@@ -8,7 +8,7 @@ public enum JLStatusCode {
     FORBIDDEN(403, "Forbidden"),
     NOT_FOUND(404, "Not Found"),
     INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
-    GENERIC_ERROR(1000, "Generic Error");
+    GENERIC_ERROR(1000, "JL Generic Error");
 
     private final int code;
     private final String message;
